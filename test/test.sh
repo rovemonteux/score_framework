c70d41bb4c43ccbe972da085e14bb57dda0aa34f
Rove Monteux <rmonteux@centrum.cz>
2017-04-06 16:13:42 +0200

    Tweaked bass.

--- test/test.sh
+++ test/test.sh
@@ -6,7 +6,7 @@ timidity --output-24bit bass_sarcofago-nightmare.mid -Ow -k0 -a -o bass_sarcofag
 timidity --output-24bit synth_sarcofago-nightmare.mid -Ow -k0 -a -o synth_sarcofago-nightmare.wav
 timidity --output-24bit guitar_sarcofago-nightmare.mid -Ow -k0 -a -o guitar_sarcofago-nightmare.wav
 sox --norm drums_sarcofago-nightmare.wav -b 32 drums_sarcofago-nightmare-temp.wav reverb rate 48000 dither -s -a 
-sox --norm bass_sarcofago-nightmare.wav -b 32 bass_sarcofago-nightmare-temp.wav rate 48000 dither -s -a
+sox --norm bass_sarcofago-nightmare.wav -b 32 bass_sarcofago-nightmare-temp.wav bass 40 rate 48000 dither -s -a
 sox --norm synth_sarcofago-nightmare.wav -b 32 synth_sarcofago-nightmare-temp.wav rate 48000 dither -s -a
 sox --norm guitar_sarcofago-nightmare.wav -b 32 guitar_sarcofago-nightmare-temp.wav overdrive rate 48000 dither -s -a
 sox -m drums_sarcofago-nightmare-temp.wav bass_sarcofago-nightmare-temp.wav synth_sarcofago-nightmare-temp.wav guitar_sarcofago-nightmare-temp.wav sarcofago-nightmare-temp.wav
