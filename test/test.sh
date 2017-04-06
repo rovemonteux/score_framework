fb4b1dbb585964e157f0c44ddc53f7dd3275d205
Rove Monteux <rmonteux@centrum.cz>
2017-04-06 15:11:01 +0200

    Added mastering to rendering.

--- test/test.sh
+++ test/test.sh
@@ -10,8 +10,9 @@ sox --norm bass_sarcofago-nightmare.wav -b 32 bass_sarcofago-nightmare-temp.wav 
 sox --norm synth_sarcofago-nightmare.wav -b 32 synth_sarcofago-nightmare-temp.wav rate 48000 dither -s -a
 sox --norm guitar_sarcofago-nightmare.wav -b 32 guitar_sarcofago-nightmare-temp.wav overdrive rate 48000 dither -s -a
 sox -m drums_sarcofago-nightmare-temp.wav bass_sarcofago-nightmare-temp.wav synth_sarcofago-nightmare-temp.wav guitar_sarcofago-nightmare-temp.wav sarcofago-nightmare-temp.wav
-sox sarcofago-nightmare-temp.wav sarcofago-nightmare.wav pad 1 1
+sox --norm sarcofago-nightmare-temp.wav -b 32 sarcofago-nightmare-master-temp.wav rate 48000 dither -s -a
+sox sarcofago-nightmare-master-temp.wav sarcofago-nightmare.wav pad 1 1
 #mv sarcofago-nightmare-temp.wav sarcofago-nightmare.wav
-rm *sarcofago-nightmare-temp.wav
+rm *-temp.wav
 rm drums_* bass_* synth_* guitar_*
 lame -q0 -b256 sarcofago-nightmare.wav sarcofago-nightmare.mp3
