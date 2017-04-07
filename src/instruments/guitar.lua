ebf6e10c0d28f976d15ebafe07d325f7d741f605
Rove Monteux <rmonteux@centrum.cz>
2017-04-07 08:12:01 +0200

    Added guitar chords.

--- src/instruments/guitar.lua
+++ src/instruments/guitar.lua
@@ -211,6 +211,70 @@ function guitar_2_15 (timeline, noteduration, notevelocity, position, channel)
 	note_f4 (timeline, noteduration, notevelocity, position, channel)
 end
 
+function guitar_3_0 (timeline, noteduration, notevelocity, position, channel)
+	note_g3 (timeline, noteduration, notevelocity, position, channel)
+end
+
+function guitar_3_1 (timeline, noteduration, notevelocity, position, channel)
+	note_gs3 (timeline, noteduration, notevelocity, position, channel)
+end
+
+function guitar_3_2 (timeline, noteduration, notevelocity, position, channel)
+	note_a3 (timeline, noteduration, notevelocity, position, channel)
+end
+
+function guitar_3_3 (timeline, noteduration, notevelocity, position, channel)
+	note_as3 (timeline, noteduration, notevelocity, position, channel)
+end
+
+function guitar_3_4 (timeline, noteduration, notevelocity, position, channel)
+	note_b3 (timeline, noteduration, notevelocity, position, channel)
+end
+
+function guitar_3_5 (timeline, noteduration, notevelocity, position, channel)
+	note_c4 (timeline, noteduration, notevelocity, position, channel)
+end
+
+function guitar_3_6 (timeline, noteduration, notevelocity, position, channel)
+	note_cs4 (timeline, noteduration, notevelocity, position, channel)
+end
+
+function guitar_3_7 (timeline, noteduration, notevelocity, position, channel)
+	note_d4 (timeline, noteduration, notevelocity, position, channel)
+end
+
+function guitar_3_8 (timeline, noteduration, notevelocity, position, channel)
+	note_ds4 (timeline, noteduration, notevelocity, position, channel)
+end
+
+function guitar_3_9 (timeline, noteduration, notevelocity, position, channel)
+	note_e4 (timeline, noteduration, notevelocity, position, channel)
+end
+
+function guitar_3_10 (timeline, noteduration, notevelocity, position, channel)
+	note_f4 (timeline, noteduration, notevelocity, position, channel)
+end
+
+function guitar_3_11 (timeline, noteduration, notevelocity, position, channel)
+	note_fs4 (timeline, noteduration, notevelocity, position, channel)
+end
+
+function guitar_3_12 (timeline, noteduration, notevelocity, position, channel)
+	note_g4 (timeline, noteduration, notevelocity, position, channel)
+end
+
+function guitar_3_13 (timeline, noteduration, notevelocity, position, channel)
+	note_gs4 (timeline, noteduration, notevelocity, position, channel)
+end
+
+function guitar_3_14 (timeline, noteduration, notevelocity, position, channel)
+	note_a4 (timeline, noteduration, notevelocity, position, channel)
+end
+
+function guitar_3_15 (timeline, noteduration, notevelocity, position, channel)
+	note_as4 (timeline, noteduration, notevelocity, position, channel)
+end
+
 function guitar_chord_0_0 (timeline, noteduration, notevelocity, position, channel)
 	guitar_0_0 (timeline, noteduration, notevelocity, position, channel)
   guitar_1_2 (timeline, noteduration, notevelocity, position, channel)
@@ -221,4 +285,118 @@ function guitar_chord_0_1 (timeline, noteduration, notevelocity, position, chann
 	guitar_0_1 (timeline, noteduration, notevelocity, position, channel)
   guitar_1_3 (timeline, noteduration, notevelocity, position, channel)
   guitar_2_3 (timeline, noteduration, notevelocity, position, channel)
+end
+
+function guitar_chord_0_2 (timeline, noteduration, notevelocity, position, channel)
+	guitar_0_2 (timeline, noteduration, notevelocity, position, channel)
+  guitar_1_4 (timeline, noteduration, notevelocity, position, channel)
+  guitar_2_4 (timeline, noteduration, notevelocity, position, channel)
+end
+
+function guitar_chord_0_3 (timeline, noteduration, notevelocity, position, channel)
+	guitar_0_3 (timeline, noteduration, notevelocity, position, channel)
+  guitar_1_5 (timeline, noteduration, notevelocity, position, channel)
+  guitar_2_5 (timeline, noteduration, notevelocity, position, channel)
+end
+
+function guitar_chord_0_4 (timeline, noteduration, notevelocity, position, channel)
+	guitar_0_4 (timeline, noteduration, notevelocity, position, channel)
+  guitar_1_6 (timeline, noteduration, notevelocity, position, channel)
+  guitar_2_6 (timeline, noteduration, notevelocity, position, channel)
+end
+
+function guitar_chord_0_5 (timeline, noteduration, notevelocity, position, channel)
+	guitar_0_5 (timeline, noteduration, notevelocity, position, channel)
+  guitar_1_7 (timeline, noteduration, notevelocity, position, channel)
+  guitar_2_7 (timeline, noteduration, notevelocity, position, channel)
+end
+
+function guitar_chord_0_6 (timeline, noteduration, notevelocity, position, channel)
+	guitar_0_6 (timeline, noteduration, notevelocity, position, channel)
+  guitar_1_8 (timeline, noteduration, notevelocity, position, channel)
+  guitar_2_8 (timeline, noteduration, notevelocity, position, channel)
+end
+
+function guitar_chord_0_7 (timeline, noteduration, notevelocity, position, channel)
+	guitar_0_7 (timeline, noteduration, notevelocity, position, channel)
+  guitar_1_9 (timeline, noteduration, notevelocity, position, channel)
+  guitar_2_9 (timeline, noteduration, notevelocity, position, channel)
+end
+
+function guitar_chord_0_8 (timeline, noteduration, notevelocity, position, channel)
+	guitar_0_8 (timeline, noteduration, notevelocity, position, channel)
+  guitar_1_10 (timeline, noteduration, notevelocity, position, channel)
+  guitar_2_10 (timeline, noteduration, notevelocity, position, channel)
+end
+
+function guitar_chord_0_9 (timeline, noteduration, notevelocity, position, channel)
+	guitar_0_9 (timeline, noteduration, notevelocity, position, channel)
+  guitar_1_11 (timeline, noteduration, notevelocity, position, channel)
+  guitar_2_11 (timeline, noteduration, notevelocity, position, channel)
+end
+
+function guitar_chord_0_10 (timeline, noteduration, notevelocity, position, channel)
+	guitar_0_10 (timeline, noteduration, notevelocity, position, channel)
+  guitar_1_12 (timeline, noteduration, notevelocity, position, channel)
+  guitar_2_12 (timeline, noteduration, notevelocity, position, channel)
+end
+
+function guitar_chord_1_1 (timeline, noteduration, notevelocity, position, channel)
+	guitar_1_1 (timeline, noteduration, notevelocity, position, channel)
+  guitar_2_3 (timeline, noteduration, notevelocity, position, channel)
+  guitar_3_3 (timeline, noteduration, notevelocity, position, channel)
+end
+
+function guitar_chord_1_2 (timeline, noteduration, notevelocity, position, channel)
+	guitar_1_2 (timeline, noteduration, notevelocity, position, channel)
+  guitar_2_4 (timeline, noteduration, notevelocity, position, channel)
+  guitar_3_4 (timeline, noteduration, notevelocity, position, channel)
+end
+
+function guitar_chord_1_3 (timeline, noteduration, notevelocity, position, channel)
+	guitar_1_3 (timeline, noteduration, notevelocity, position, channel)
+  guitar_2_5 (timeline, noteduration, notevelocity, position, channel)
+  guitar_3_5 (timeline, noteduration, notevelocity, position, channel)
+end
+
+function guitar_chord_1_4 (timeline, noteduration, notevelocity, position, channel)
+	guitar_1_4 (timeline, noteduration, notevelocity, position, channel)
+  guitar_2_6 (timeline, noteduration, notevelocity, position, channel)
+  guitar_3_6 (timeline, noteduration, notevelocity, position, channel)
+end
+
+function guitar_chord_1_5 (timeline, noteduration, notevelocity, position, channel)
+	guitar_1_5 (timeline, noteduration, notevelocity, position, channel)
+  guitar_2_7 (timeline, noteduration, notevelocity, position, channel)
+  guitar_3_7 (timeline, noteduration, notevelocity, position, channel)
+end
+
+function guitar_chord_1_6 (timeline, noteduration, notevelocity, position, channel)
+	guitar_1_6 (timeline, noteduration, notevelocity, position, channel)
+  guitar_2_8 (timeline, noteduration, notevelocity, position, channel)
+  guitar_3_8 (timeline, noteduration, notevelocity, position, channel)
+end
+
+function guitar_chord_1_7 (timeline, noteduration, notevelocity, position, channel)
+	guitar_1_7 (timeline, noteduration, notevelocity, position, channel)
+  guitar_2_9 (timeline, noteduration, notevelocity, position, channel)
+  guitar_3_9 (timeline, noteduration, notevelocity, position, channel)
+end
+
+function guitar_chord_1_8 (timeline, noteduration, notevelocity, position, channel)
+	guitar_1_8 (timeline, noteduration, notevelocity, position, channel)
+  guitar_2_10 (timeline, noteduration, notevelocity, position, channel)
+  guitar_3_10 (timeline, noteduration, notevelocity, position, channel)
+end
+
+function guitar_chord_1_9 (timeline, noteduration, notevelocity, position, channel)
+	guitar_1_9 (timeline, noteduration, notevelocity, position, channel)
+  guitar_2_11 (timeline, noteduration, notevelocity, position, channel)
+  guitar_3_11 (timeline, noteduration, notevelocity, position, channel)
+end
+
+function guitar_chord_1_10 (timeline, noteduration, notevelocity, position, channel)
+	guitar_1_10 (timeline, noteduration, notevelocity, position, channel)
+  guitar_2_12 (timeline, noteduration, notevelocity, position, channel)
+  guitar_3_12 (timeline, noteduration, notevelocity, position, channel)
 end
