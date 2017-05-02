ba7834a5a78ea89b55f50d9e4bd387949f8620c1
Rove Monteux <rmonteux@centrum.cz>
2017-05-02 17:01:45 +0300

    Updated install.sh to make use of sudo.

--- soundfonts/install.sh
+++ soundfonts/install.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
-cp *.sf2 /usr/share/sounds/sf2/
-apt-get install timidity
-apt-get install fluid-soundfont-gm
-apt-get install fluid-soundfont-gs
-cp rove.cfg /etc/timidity/
-cp timidity.cfg /etc/timidity/
-apt-get install normalize-audio
-apt-get install sox
-apt-get install lame
-apt-get install curl
-apt-get install libcurl4-openssl-dev
-apt-get install mpg123
-luarocks install midi
-luarocks install luacurl
-luarocks install Lua-cURL --server=https://rocks.moonscript.org/dev 
+sudo cp *.sf2 /usr/share/sounds/sf2/
+sudo apt-get install timidity
+sudo apt-get install fluid-soundfont-gm
+sudo apt-get install fluid-soundfont-gs
+sudo cp rove.cfg /etc/timidity/
+sudo cp timidity.cfg /etc/timidity/
+sudo apt-get install normalize-audio
+sudo apt-get install sox
+sudo apt-get install lame
+sudo apt-get install curl
+sudo apt-get install libcurl4-openssl-dev
+sudo apt-get install mpg123
+sudo apt-get install luarocks
+sudo luarocks install midi
+sudo luarocks install luacurl
+sudo luarocks install Lua-cURL --server=https://rocks.moonscript.org/dev 
